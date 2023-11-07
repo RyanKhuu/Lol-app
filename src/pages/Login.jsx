e08bc24d1311759e744ca37217a615054d1b3196
RyanKhuu <85713643+RyanKhuu@users.noreply.github.com>
2023-11-07 01:51:20 -0500

    Added Login Page Html
    Added login page html and changed some classnames

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 
-const SignIn = () => {
+const Login = () => {
     return(
-        <div className="SulPage">
+        <div class="sulPage">
             <div className="sulContainer">
                 <div className="sulItems">
                     <div className="EmailPassword">
                         <img></img>
                         <form>
-                            <h4 className="SUL">Create an Account</h4>
-                            <div class="formAttributes">
-                                <label for="UserDisplayName">Display Name</label>
-                                <input type="text" id="UserDisplayName" name="DisplayName"></input>
-                            </div>
+                            <h4 className="SUL">Login</h4>
                             <div class="formAttributes">
                                 <label for="UserEmail">Email</label>
                                 <input type="text" id="UserEmail" name="email"></input>
@@ -22,7 +18,7 @@ const SignIn = () => {
                                 <input type="text" id="UserPassword" name="Password"></input>
                             </div>
                             <div class="formButton">
-                                <button class="sulButton">Create Account</button>
+                                <button class="sulButton">Login</button>
                             </div>
                         </form>
                     </div>
@@ -32,4 +28,4 @@ const SignIn = () => {
     )
 }
 
-export default SignIn
+export default Login
